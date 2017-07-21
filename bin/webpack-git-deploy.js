b776a2ae08676a337cb4853bd55e03a4aa2d7ea9
Frank Lyder Bredland <fl@nyggen.com>
2017-07-21 14:07:49 +0200

    Do webpack build and gennerate a pack-file
    TODO: implement push to remote and update remote ref

--- bin/webpack-git-deploy.js
+++ bin/webpack-git-deploy.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-
+const MemoryFS = require("memory-fs");
 const webpack = require("webpack");
+
+const AUTHOR_NAME='Webpack-git-deploy';
+const AUTHOR_EMAIL='noreply@theknarf.com';
 
 const yargs = require("yargs");
 require("webpack/bin/config-yargs")(yargs);
@@ -8,6 +11,8 @@ require("webpack/bin/config-yargs")(yargs);
 const argv = yargs.argv;
 
 const wpOpt = require("webpack/bin/convert-argv")(yargs, argv);
+
+wpOpt.output.path = '/';
 
 function processOptions(wpOpt) {
 	let compiler;
@@ -21,28 +26,77 @@ function processOptions(wpOpt) {
 		throw e;
 	}
 
-	compiler.plugin("done", (stats) => {
-		/*		
-		var cache = [];
-		console.log(JSON.stringify(stats.compilation.assets, function(key, value) {
-		    if (typeof value === 'object' && value !== null) {
-		        if (cache.indexOf(value) !== -1) {
-		            // Circular reference found, discard key
-		            return;
-		        }
-		        // Store value in our collection
-		        cache.push(value);
-		    }
-		    return value;
-		}));//*/
+	// This provides symbolic names for the octal modes used by git trees.
+	var modes = require('js-git/lib/modes');
 
+	// Create a repo by creating a plain object.
+	var repo = {};
 
-		Object.keys(stats.compilation.assets).forEach(function(key) {
-			console.log(key);
+	require('js-git/mixins/mem-db')(repo);
+	require('js-git/mixins/create-tree')(repo);
+	require('js-git/mixins/pack-ops')(repo);
+	require('js-git/mixins/walkers')(repo);
+	require('js-git/mixins/read-combiner')(repo);
+	require('js-git/mixins/formats')(repo);
+
+	const fs = new MemoryFS();
+	compiler.outputFileSystem = fs;
+
+	compiler.run((err, stats) => {
+		var tree = {};
+		var hashes = [];
+
+		function debugFs(filename) {
+			console.log(filename)
+
+			if(fs.statSync(filename).isDirectory()) {
+				fs
+					.readdirSync(filename)
+					.forEach((filename2) => debugFs(filename + filename2 + "/" ));
+			}
+		}
+		//debugFs('/');
+
+		Object.keys(stats.compilation.assets).forEach((filename) => {
+			const filecontent = fs.readFileSync('/' + filename);
+			
+			repo.saveAs("blob", filecontent, (err, blobHash) => {
+				if (err) throw err;
+
+				tree[filename] = { mode: modes.file, hash: blobHash };
+				hashes.push(blobHash);
+			});
 		});
 
-	});
+		repo.saveAs("tree", tree, (err, treeHash) => {
+			if (err) throw err;
+			hashes.push(treeHash);
 
-	compiler.run(()=>{});
+			repo.saveAs("commit", {
+			    author: { name: AUTHOR_NAME, email: AUTHOR_EMAIL },
+			    tree: treeHash,
+			    message: "Webpack build\n"
+			}, (err, commitHash) => {
+				if (err) throw err;
+
+				hashes.push(commitHash);
+				
+				repo.pack(hashes, {}, (err, stream) => {
+					if (err) throw err;
+
+					function readStream() {
+						stream.take((err, data) => {
+							if(err) throw err;
+							if(data === undefined) return;
+							process.stdout.write(data);
+							readStream();
+						});
+					}
+					readStream();
+				});
+			});
+		});
+	});
 }
+
 processOptions(wpOpt);
