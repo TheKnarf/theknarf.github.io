7aad758c0f0708be95900b08150f4480dbeeb58a
Frank Lyder Bredland <fl@nyggen.com>
2019-07-17 17:04:22 +0200

    Added 404 page handeling

--- packages/ravendesk-cli/src/command/dev.js
+++ packages/ravendesk-cli/src/command/dev.js
@@ -1,5 +1,6 @@
 const middleware = require('webpack-dev-middleware'),
-		express = require('express');
+		express = require('express'),
+		path = require('path');
 
 const { setupCompiler } = require('../helpers/compiler.js');
 
@@ -11,6 +12,23 @@ const action = async () => {
 
 	const app = express();
 	app.use( middleware(compiler, {}) );
+
+	app.use((req, res) => {
+		res.status(404);
+		res.set('content-type','text/html');
+
+		const filename = path.join(compiler.outputPath,'404.html');
+		compiler.outputFileSystem.readFile(filename, (err, result) => {
+			if(err) {
+				console.error(`Can't find custom 404.html page, just send plain 404 page`);
+				res.send('<h1>404</h1>');
+			} else {
+				res.send(result);
+			}
+
+			res.end();
+		});
+	});
 
 	const port=3000;
 	await app.listen(port);
