015ace8c8026927463b14d4ba218207bc62c99a1
Frank Lyder Bredland <fl@nyggen.com>
2019-07-19 16:06:10 +0200

    Beginning of mdx support added back inn

--- packages/ravendesk-preset-blog/src/index.js
+++ packages/ravendesk-preset-blog/src/index.js
@@ -4,22 +4,26 @@ module.exports = ({ config, mode }) => {
 	config.target = 'node';
 	config.externals = [ require('webpack-node-externals')() ];
 
+	const babelLoader = { loader: 'babel-loader', options: {
+		presets: [
+			[ require.resolve('@babel/preset-react'), {
+				pragma: 'dom',
+				pragmaFrag: 'fragment',
+				throwIfNamespace: false
+			}]
+		]
+	}};
+
 	config.module.rules = [
 		{
 			test: /\.jsx?$/,
 			exclude: /node_modules/,
-			use: [
-				{ loader: 'babel-loader', options: {
-					presets: [
-						[ require.resolve('@babel/preset-react'), {
-							pragma: 'dom',
-							pragmaFrag: 'fragment',
-							throwIfNamespace: false
-						}]
-					]
-				}},
-				'simple-frontmatter-loader',
-			]
+			use: [ babelLoader, 'simple-frontmatter-loader' ]
+		},
+		{
+			test: /\.mdx?$/,
+			exclude: /node_modules/,
+			use: [ babelLoader, 'ravendesk-mdx-loader', 'simple-frontmatter-loader' ]
 		},
 		{
 			test: /\.html$/,
