6a78ad3d76bfbaa667a4e3950bb5aaecc44441d0
Frank Lyder Bredland <fl@nyggen.com>
2020-05-22 19:53:04 +0200

    Simplify html

--- packages/theknarf-com/src/index.js
+++ packages/theknarf-com/src/index.js
@@ -2,26 +2,22 @@
 route: 'index.html'
 ---
 
-import { dom } from 'isomorphic-jsx';
+import { dom, fragment } from 'isomorphic-jsx';
 import config from './config';
 import github from './github.svg';
 
-import Page from './default'; 
+import Page from './default';
 export const layout = Page;
 
-export default <div>
+export default <>
   <span class="slogan">[this page is left intentionally blank]</span>
-	<div class="center-vertical">
-		<div class="profile">
-			<div class="info">
-				<a class="github fa fa-github" href="https://github.com/TheKnarf">
-					<img src={github} alt="github logo" />
-					<span>Github</span>
-				</a>
-				<a href={'http://experiments.' + config.domain}>Experiments</a>
-				<a href={'http://notes.' + config.domain}>Notes</a>
-				<a href={'http://talks.' + config.domain}>Talks</a>
-			</div>
-		</div>
+	<div>
+		<a href="https://github.com/TheKnarf">
+			<img src={github} alt="github logo" />
+			<span>Github</span>
+		</a>
+		<a href={'http://experiments.' + config.domain}>Experiments</a>
+		<a href={'http://notes.' + config.domain}>Notes</a>
+		<a href={'http://talks.' + config.domain}>Talks</a>
 	</div>
-</div>
+</>;
