636fb588e4b9ec03c3e10daf98871b3675049663
Frank Lyder Bredland <fl@nyggen.com>
2023-11-15 17:19:53 +0100

    Port website over to Astro

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const page = {
-	title: 'TheKnarf',
-	description: '',
-};
-
-export default {
-	page,
-	domain: 'theknarf.com' // change this based on if its a debug build or not
-};
--- src/default.js
+++
@@ -1,9 +0,0 @@
-import { dom } from 'isomorphic-jsx';
-import Head from './head';
-
-export default ({children}) => <html>
-	<Head />
- 	<body>
-		{ children }
-	</body>
-</html>
